79fbe25995d29969c4fe79e7a5b594b8b70cf3fb
FiestaLake <FiestaLake@protonmail.com>
2020-08-28 00:21:23 +0900

    klteduos: ResurrectionRemix-ify
    Signed-off-by: Sung Mingi <FiestaLake@protonmail.com>

--- rr_klteduos.mk
+++ rr_klteduos.mk
@@ -1,5 +1,7 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2020 The ResurrectionRemix Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,17 +14,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_klteduos.mk \
-    $(LOCAL_DIR)/rr_klteduos.mk
+# Inherit some common ResurrectionRemix stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
-COMMON_LUNCH_CHOICES := \
-    lineage_klteduos-user \
-    lineage_klteduos-userdebug \
-    lineage_klteduos-eng
+$(call inherit-product, device/samsung/klteduos/full_klteduos.mk)
 
-COMMON_LUNCH_CHOICES := \
-    rr_klteduos-user \
-    rr_klteduos-userdebug \
-    rr_klteduos-eng
+PRODUCT_DEVICE := klteduos
+PRODUCT_NAME := rr_klteduos
